a46eefb7d3e1b4689532922e4203f214714759b6
j-0-n-e-z <j000neeez@gmail.com>
2023-04-27 19:30:52 +0500

    add desktop markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [email, setEmail] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function handleEmailSubmit(e: FormEvent) {
+		e.preventDefault()
+		return Math.PI
+	}
+
+	return (
+		<main className='font-["Josefin_Sans"] h-screen min-h-screen bg-gradientWhite'>
+			<section className='flex h-full'>
+				<div className='w-7/12 px-[164px] py-16 bg-pattern bg-no-repeat bg-cover'>
+					<img src='./assets/images/logo.svg' alt='logo' />
+					<h1 className='flex flex-col mt-[134px]'>
+						<span className='heading font-light text-desaturatedRed'>
+							We're
+						</span>
+						<span className='heading font-semibold text-darkGrayishRed'>
+							coming soon
+						</span>
+					</h1>
+					<p className='ml-[1px] mt-[22px] pr-[4rem] text-desaturatedRed text-[16px] tracking-[0.004rem] leading-[1.7rem]'>
+						Hello fellow shoppers! We're currently building our new fashion
+						store. Add your email below to stay up-to-date with announcements
+						and our launch deals.
+					</p>
+					<div className='mt-10'>
+						<form className='flex relative w-[calc(100%-4rem)]'>
+							<input
+								className='outline-none text-darkGrayishRed  placeholder-desaturatedRed/50  bg-transparent border-[1px] border-desaturatedRed/50 rounded-full px-8 py-4 w-full'
+								type='email'
+								name='email'
+								placeholder='Email Address'
+								value={email}
+								onChange={e => setEmail(e.target.value)}
+							/>
+							<button
+								type='submit'
+								className='absolute right-0 bg-gradientRed hover:bg-gradientHover transition rounded-full grid place-items-center px-[43px] py-[19px] shadow-[0_10px_25px_0_#ce9797]'
+								onClick={handleEmailSubmit}
+							>
+								<img src='./assets/images/icon-arrow.svg' alt='arrow' />
+							</button>
+						</form>
+					</div>
+				</div>
+				<div className='h-full'>
+					<img
+						className='h-full w-full object-cover'
+						src='./assets/images/hero-desktop.jpg'
+						alt='hero'
+					/>
+				</div>
+			</section>
+		</main>
+	)
 }
-
-export default App
