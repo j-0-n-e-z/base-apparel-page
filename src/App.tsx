ac0df65e1b7e3db98d63bba7558c58c74a016eb4
j-0-n-e-z <j000neeez@gmail.com>
2023-05-13 15:12:16 +0500

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
 				.max(30, 'Email cannot be longer than 30 characters')
 		}),
 		onSubmit: () => {
-			console.log(formik.values)
+			console.log(formik.values.email)
 			formik.resetForm()
 		}
 	})
@@ -22,15 +22,17 @@ export default function App() {
 	return (
 		<main className='font-JosefinSans h-screen min-h-screen bg-gradientWhite'>
 			<section className='flex lg:flex-row flex-col h-full'>
-				<div className='lg:w-7/12 lg:px-[164px] lg:py-16 lg:bg-pattern bg-no-repeat bg-cover'>
+				<div className='lg:w-7/12 lg:px-20 xl:px-[164px] sm:px-16 lg:py-16 lg:bg-pattern bg-no-repeat bg-cover'>
 					<img
 						className='lg:w-auto w-24 lg:m-0 ml-8 my-8'
 						src='./assets/images/logo.svg'
 						alt='logo'
 					/>
-					<div className='lg:hidden flex justify-center'>
-						<img src='./assets/images/hero-mobile.jpg' alt='hero' />
-					</div>
+					<img
+						className='lg:hidden block mx-auto'
+						src='./assets/images/hero-mobile.jpg'
+						alt='hero'
+					/>
 					<h1 className='flex flex-col lg:text-start text-center lg:mt-[134px] mt-[62px]'>
 						<span className='heading font-light text-desaturatedRed'>
 							We're
@@ -39,14 +41,14 @@ export default function App() {
 							coming soon
 						</span>
 					</h1>
-					<p className='lg:text-start text-center ml-[1px] lg:mt-[22px] mt-[15px] lg:pr-[4rem] lg:pl-0 px-7 text-desaturatedRed lg:text-[16px] text-[14px] tracking-[0.004rem] lg:leading-[1.7rem] leading-[1.4rem]'>
+					<p className='lg:text-start text-center ml-[1px] lg:mt-[22px] mt-[15px] xl:pr-[4rem] lg:px-0 px-7 text-desaturatedRed lg:text-[16px] text-[14px] tracking-[0.004rem] lg:leading-[1.7rem] leading-[1.4rem]'>
 						Hello fellow shoppers! We're currently building our new fashion
 						store. Add your email below to stay up-to-date with announcements
 						and our launch deals.
 					</p>
 					<div className='lg:mt-10 mt-[30px] lg:p-0 px-10'>
 						<form
-							className='flex relative lg:w-[calc(100%-4rem)]'
+							className='flex relative xl:w-[calc(100%-4rem)]'
 							onSubmit={formik.handleSubmit}
 						>
 							<input
@@ -78,15 +80,15 @@ export default function App() {
 							)}
 						</form>
 						{formik.errors.email && formik.touched.email && (
-							<p className='self-start text-softRed text-sm mt-3 lg:ml-8 ml-7'>
+							<small className='block self-start text-softRed text-sm mt-3 lg:ml-8 ml-7'>
 								{formik.errors.email}
-							</p>
+							</small>
 						)}
 					</div>
 				</div>
-				<div className='h-full lg:block hidden'>
+				<div className='lg:block hidden'>
 					<img
-						className='h-full w-full object-cover'
+						className='h-full object-cover object-left'
 						src='./assets/images/hero-desktop.jpg'
 						alt='hero'
 					/>
